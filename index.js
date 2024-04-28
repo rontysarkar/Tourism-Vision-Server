b552536456f66a80273927117741cd6ca85f7679
rontysarkar <rontysarkar2023@gmail.com>
2024-04-28 14:05:01 +0600

    add get on element method

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(cors({
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dam4d01.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -51,6 +51,13 @@ async function run() {
         const cursor = touristsCollection.find()
         const result = await cursor.toArray()
         res.send(result)
+    })
+
+    app.get('/tourists/:id',async(req,res)=>{
+        const id = req.params
+        const query = { _id: new ObjectId(id)}
+        const result = await touristsCollection.findOne(query)
+        res.send(res)
     })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
