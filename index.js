37f3c4c0d23b6b978bb8da2729b213991ac90040
rontysarkar <rontysarkar2023@gmail.com>
2024-04-30 12:08:28 +0600

    add all oparation

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
+    
     // await client.connect();
 
     const touristsCollection =client.db('touristsDB').collection('touristsSpot');
