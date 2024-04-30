fe0013b15638a8e5ada0378bd34f1db261b27ce8
rontysarkar <rontysarkar2023@gmail.com>
2024-04-30 13:45:15 +0600

    add origin in firebase url

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 5000;
 
 app.use(express.json())
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: ['http://localhost:5173','https://tourism-vision.web.app'],
     methods:["GET","POST","PUT","DELETE"],
 }));
 
